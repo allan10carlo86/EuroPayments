e6ecea51b6c7f364461fa2e8fc081de8ab0441d0
Allan Ramos <allan.ramos@iselect.com.au>
2020-07-04 20:48:17 +0800

    commit to jar

---
+++ payments/src/main/java/framework/main/MainClass.java
@@ -0,0 +1,20 @@
+package framework.main;
+
+import java.util.List;
+
+import org.testng.TestListenerAdapter;
+import org.testng.TestNG;
+import org.testng.collections.Lists;
+
+public class MainClass {
+	public static void main(String[] args) {
+		TestListenerAdapter tla = new TestListenerAdapter();
+		TestNG testNG = new TestNG();	
+		List<String> suites = Lists.newArrayList();
+		suites.add("testng.xml");
+		
+		testNG.setTestSuites(suites);
+		testNG.run();
+		
+	}
+}
